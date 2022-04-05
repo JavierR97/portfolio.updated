76116759d198397ed8aedf228bcaa35f7be07a9d
JavierR97 <javierrivera.webdev@gmail.com>
2022-04-05 19:24:22 -0400

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+var toggle = document.querySelector('.sandwich-menu');
+let navLinks = document.querySelector('.nav-list');
+
+toggle.addEventListener('click', () => {
+    document.getElementsByClassName('nav-list')[0].classList.toggle('active')
+})
+
+
+
+
+
+
+
