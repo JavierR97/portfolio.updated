75d95705165877310158f80d6c21f265c2b43d11
JavierR97 <javierrivera.webdev@gmail.com>
2022-04-06 12:08:29 -0400

    added comment

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
+// add toggle functionality to sandwitch-menu
 var toggle = document.querySelector('.sandwich-menu');
-
 toggle.addEventListener('click', () => {
     document.getElementsByClassName('nav-list')[0].classList.toggle('active')
 })
