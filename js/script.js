2939a852cb204956e8aebf589725f10f0af6f782
JavierR97 <javierrivera.webdev@gmail.com>
2022-04-06 12:08:11 -0400

    removed unnecessary code / added comment

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 var toggle = document.querySelector('.sandwich-menu');
-let navLinks = document.querySelector('.nav-list');
 
 toggle.addEventListener('click', () => {
     document.getElementsByClassName('nav-list')[0].classList.toggle('active')
